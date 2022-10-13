157091e253a7086756ca73d864bbfbf6014b4274
iron <iron@menuit.io>
2022-10-13 16:37:16 +0900

    applied setting for aws

--- dwm/config/settings.py
+++ dwm/config/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print("dwm setting BASE_DIR :",BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-mbfhtzxhtoy+-s^4c+mu!g%y@mpskmm-ojdsbvg3yy3t5=3rgl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+	".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
